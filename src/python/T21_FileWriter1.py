b7c6effb67753b275f3ab37509537aa1c86923ad
Aryan Prajapati <aryangithub2017@gmail.com>
2022-11-20 13:52:28 +0530

    Initial Commit

---
+++ src/python/T21_FileWriter1.py
@@ -0,0 +1,12 @@
+file = open("T21_FileWriting.txt", "a")  # here "a" mode allows you to append the ontent
+file.write("Anup is the friend of Aryan\n")
+file.write("Karthika is the friend of Anup\n")
+file.close()
+
+
+file1 = open("T21_FileWriting.txt", "r+")  # to read and write both
+content = file1.read()
+print(content)
+file1.write("Thank you")
+print(content)
+file1.close()
