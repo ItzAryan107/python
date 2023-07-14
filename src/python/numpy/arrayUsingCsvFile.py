614654e6fb6a3f5c24fda0314992c625953752f3
Aryan Prajapati <aryangithub2017@gmail.com>
2023-07-15 01:36:55 +0530

    Initial Commit

--- src/python/numpy/arrayUsingCsvFile.py
+++
@@ -1,7 +0,0 @@
-from numpy import loadtxt as lt
-
-print("3. ---> Reading array from disk, either from standard or custom formats")
-
-arr = lt('simple.csv', delimiter=',', skiprows=1)
-print(arr)
-print(type(arr))
